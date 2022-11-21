4d9305fcb894a65ab2fabd39d8dd4b3df372e316
Miku <26039249+xMikux@users.noreply.github.com>
2022-11-21 09:44:36 +0000

    改版腳本

--- .github/scripts/ModPacker.sh
+++ .github/scripts/ModPacker.sh
@@ -8,26 +8,61 @@
 ### 設定區 ###
 
 ## 模組包名稱
+
 ModPackName=FTB-StoneBlock-3
 
+## 是否生成「模組中文化資源包」
+
+ModsResourcePack_Generate=false
+
 ## 要建立進模組包漢化補丁中的陣列
-ConfigPath=(config kubejs)
 
-### 腳本執行區 ###
+ConfigPath=(config kubejs LICENSE)
 
-# 宣告模組包變數名
-echo "modpack_name=$ModPackName" >> "$GITHUB_OUTPUT"
+### 函數區 ###
 
-# 設置回主工作區
-cd "$GITHUB_WORKSPACE" || exit
+## GitHub 環境設置
 
-# 製作一個臨時資料夾
-workdir="$(mktemp -d)"
+environment_setup () {
+  ## 宣告模組包變數
+  
+  echo "modpack_name=$ModPackName" >> "$GITHUB_OUTPUT"
 
-# 複製內容到臨時資料夾
-for path in "${ConfigPath[@]}"; do
-  cp -r "$path" "$workdir/"
-done
+  ## 宣告生成資源包變數
+  
+  echo "mods_gen=$ModsResourcePack_Generate" >> "$GITHUB_OUTPUT"
+}
 
-# 壓縮並製作模組包漢化補丁
-cd $workdir && zip -r "$OLDPWD/$ModPackName-Patches-$VERSION.zip" *
+## 製作補丁
+
+patch_maker () {
+  ## 設置回主工作區
+  
+  cd "$GITHUB_WORKSPACE" || exit
+
+  ## 製作一個臨時資料夾
+  
+  workdir="$(mktemp -d)"
+
+  ## 複製內容到臨時資料夾
+  
+  for path in "${ConfigPath[@]}"; do
+    cp -r "$path" "$workdir/"
+  done
+
+  ## 壓縮並製作模組包漢化補丁
+  
+  cd "$workdir" && zip -r "$OLDPWD/$ModPackName-Patches-$VERSION.zip" *
+}
+
+
+### 主腳本執行 ###
+
+if [ "$1" = 1 ]; then
+  environment_setup
+elif [ "$1" = 2 ]; then
+  patch_maker
+else
+  echo "錯誤，需指定執行碼！"
+  exit 1
+fi
