a2579e8213143c9a52be15dc499a00e380b39c57
xMikux <26039249+xMikux@users.noreply.github.com>
2023-01-09 04:15:28 +0800

    調整翻譯

--- kubejs/server_scripts/_core/inworldevent/events/tea.js
+++ kubejs/server_scripts/_core/inworldevent/events/tea.js
@@ -3,6 +3,6 @@ const teaEvent = {
   name: "ftbstoneblock:tea",
   chance: 0.1,
   execute(event, player, location) {
-    player.tell(["[伺服器]，來一杯茶吧，休息一下！"]);
+    player.tell(["[伺服器] 放下遊戲，來杯茶ㄅ"]);
   },
 };
